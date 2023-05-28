0f6ccf8f5ad634ca790a2bed53a478d2ffd7e367
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-28 23:06:08 +0900

    ADD : Swiper 추가

--- src/Main.js
+++ src/Main.js
@@ -1,93 +1,148 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 import { useNavigate } from "react-router-dom";
 import "./Main.css";
-
+import Swipe from "./pages/Swiper";
 function Main() {
   let [right, setRight] = useState(false);
   let navigate = useNavigate();
 
   return (
-    <div className="main">
-      <ReactPlayer
-        url={process.env.PUBLIC_URL + "/forest.mp4"}
-        width="100%"
-        height="vh"
-        playing={true}
-        muted={true}
-        loop={true}
-      />
-      <div className="vision-box">
-        <h1>OUR VISION</h1>
-        <p>
-          우리는 모든 사람에게 건강하고 아름다운 식물을 제공하여 삶의 질을
-          향상시키고자 합니다.
-        </p>
-        <button className="btn" onClick={()=>{
-          navigate('/vision')
-        }}>버튼</button>
-      </div>
-      <div className="new">
-        <h1>이번주 새로운 식물소식</h1>
-        <p>새로들어온 식물</p>
-      </div>
-      <button
-        className="btn"
-        onClick={() => {
-          setRight(false);
-        }}
-      >
-        &lt;
-      </button>
-      <button
-        className="btn"
-        onClick={() => {
-          setRight(true);
-        }}
-      >
-        &gt;
-      </button>
-      <div className="overflow">
-        <div className={right ? "new-plant right" : "new-plant"}>
-          <div className="plant">
-            <div className="img">
-              <img src="/plant1.jpg" alt="" />
+    <>
+      <div className="main">
+        <ReactPlayer
+          url={process.env.PUBLIC_URL + "/forest.mp4"}
+          width="100%"
+          height="vh"
+          playing={true}
+          muted={true}
+          loop={true}
+        />
+        <div className="vision-box">
+          <h1>OUR VISION</h1>
+          <p>
+            우리는 모든 사람에게 건강하고 아름다운 식물을 제공하여 삶의 질을
+            향상시키고자 합니다.
+          </p>
+          <button
+            className="btn"
+            onClick={() => {
+              navigate("/vision");
+            }}
+          >
+            버튼
+          </button>
+        </div>
+        <div className="new">
+          <h1>이번주 새로운 식물소식</h1>
+          <p>새로들어온 식물</p>
+        </div>
+        <button
+          className="btn"
+          onClick={() => {
+            setRight(false);
+          }}
+        >
+          &lt;
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            setRight(true);
+          }}
+        >
+          &gt;
+        </button>
+        <div className="overflow">
+          <div className={right ? "new-plant right" : "new-plant"}>
+            <div className="plant">
+              <div className="img">
+                <img src="/plant1.jpg" alt="" />
+              </div>
+              <p>나무</p>
+              <button className="btn">버튼</button>
             </div>
-            <p>나무</p>
-            <button className="btn">버튼</button>
+            <div className="plant">
+              <div className="img">
+                <img src="/plant3.jpg" alt="" />
+              </div>
+              <p>나무</p>
+              <button className="btn">버튼</button>
+            </div>
+            <div className="plant">
+              <div className="img">
+                <img src="/plant2.jpg" alt="" />
+              </div>
+              <p>나무</p>
+              <button className="btn">버튼</button>
+            </div>
+            <div className="plant">
+              <div className="img">
+                <img src="/plant4.jpg" alt="" />
+              </div>
+              <p>나무</p>
+              <button className="btn">버튼</button>
+            </div>
+            <div className="plant">
+              <div className="img">
+                <img src="/plant1.jpg" alt="" />
+              </div>
+              <p>나무</p>
+              <button className="btn">버튼</button>
+            </div>
           </div>
-          <div className="plant">
-            <div className="img">
+          <Swipe />
+        </div>
+        <div>
+          <h1>Trending Now</h1>
+          <div className="trending">
+            <div className="trending-box">
+              <img src="/plant1.jpg" alt="" />
+              <div className="trending-content">
+                <p>나무임</p>
+                <button className="btn white">구매</button>
+              </div>
+            </div>
+            <div className="trending-box">
               <img src="/plant3.jpg" alt="" />
+              <div className="trending-content">
+                <p>나무임</p>
+                <button className="btn white">구매</button>
+              </div>
             </div>
-            <p>나무</p>
-            <button className="btn">버튼</button>
-          </div>
-          <div className="plant">
-            <div className="img">
-              <img src="/plant2.jpg" alt="" />
-            </div>
-            <p>나무</p>
-            <button className="btn">버튼</button>
-          </div>
-          <div className="plant">
-            <div className="img">
-              <img src="/plant4.jpg" alt="" />
-            </div>
-            <p>나무</p>
-            <button className="btn">버튼</button>
-          </div>
-          <div className="plant">
-            <div className="img">
-              <img src="/plant1.jpg" alt="" />
-            </div>
-            <p>나무</p>
-            <button className="btn">버튼</button>
           </div>
         </div>
       </div>
-      <div style={{ height: "100px" }}></div>
-    </div>
+      <footer className="footer">
+        <div className="footer-flex">
+          <div>
+            <li>New</li>
+            <li>Member</li>
+            <li>Location</li>
+            <li>Gernal</li>
+          </div>
+          <div>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+          </div>
+          <div>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+          </div>
+          <div className="icon">
+            아이콘 4개 정렬
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
 
---
+++ src/pages/Swiper.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import "./Swiper.css"
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+
+function Swipe() {
+  return (
+    <div className="container">
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+        <div>
+        <img src="/plant1.jpg" alt="slide_image" />
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/plant2.jpg" alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/plant3.jpg" alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/plant4.jpg" alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/plant4.jpg" alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/plant4.jpg" alt="slide_image" />
+        </SwiperSlide>
+
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+    </div>
+  );
+}
+
+export default Swipe;
